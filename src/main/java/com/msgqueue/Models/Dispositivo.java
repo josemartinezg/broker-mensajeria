03108982ef5e6ad588e2c78cbf356b6053f1b5f9
Jose Manuel Martinez <2015378@ce.pucmm.edu.do>
2020-06-28 22:44:16 -0400

    Create Dispositivo.java

---
+++ src/main/java/com/msgqueue/Models/Dispositivo.java
@@ -0,0 +1,50 @@
+package com.msgqueue.Models;
+
+public class Dispositivo {
+    private String fecha;
+    private int idDispositivo;
+    private double temperatura;
+    private double humedad;
+
+    public Dispositivo() {
+    }
+
+    public Dispositivo(String fecha, int idDispositivo, double temperatura, double humedad) {
+        this.fecha = fecha;
+        this.idDispositivo = idDispositivo;
+        this.temperatura = temperatura;
+        this.humedad = humedad;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getIdDispositivo() {
+        return idDispositivo;
+    }
+
+    public void setIdDispositivo(int idDispositivo) {
+        this.idDispositivo = idDispositivo;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public double getHumedad() {
+        return humedad;
+    }
+
+    public void setHumedad(double humedad) {
+        this.humedad = humedad;
+    }
+}
