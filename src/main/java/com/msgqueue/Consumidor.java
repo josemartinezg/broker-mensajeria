05c769c7549c2d3243cc165685e7fa76968e1f23
Jose Manuel Martinez <2015378@ce.pucmm.edu.do>
2020-07-02 08:09:52 -0400

    Probando implementación Rabbit MQ

--- src/main/java/com/msgqueue/Consumidor.java
+++ src/main/java/com/msgqueue/Consumidor.java
@@ -1,5 +1,4 @@
 package com.msgqueue;
-import com.sun.tools.javac.Main;
 import freemarker.template.Configuration;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
@@ -12,40 +11,50 @@ import com.rabbitmq.client.DeliverCallback;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import static spark.Spark.staticFiles;
 import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class Consumidor {
     private static final String EXCHANGE_NAME = "logs";
     static Gson gson = new Gson();
 
     public static void main(String[] argv) throws Exception {
-
+        port(8080);
         Configuration configuration = new Configuration(Configuration.getVersion());
-        configuration.setClassForTemplateLoading(Main.class, "/publico/templates");
+        staticFiles.location("/publico/dist");
+        configuration.setClassForTemplateLoading(Consumidor.class, "/publico/templates");
         FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(configuration);
         Map<String, Object> attributes = new HashMap<>();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" Esperando mensajes... ");
+
         get("/charts", (request, response) -> {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-
-            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-            String queueName = channel.queueDeclare().getQueue();
-            channel.queueBind(queueName, EXCHANGE_NAME, "");
-
-            System.out.println(" Esperando mensajes... ");
-
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
                 String mensaje = new String(delivery.getBody(), "UTF-8");
                 System.out.println(" Recibido: '" + mensaje + "'");
                 Dispositivo dispositivo = gson.fromJson(mensaje, Dispositivo.class);
                 System.out.println(" Recibido: '" + dispositivo.getFecha() + "'");
                 attributes.put("dispositivo", dispositivo);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                response.redirect("/charts");
             };
             channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
-            return new ModelAndView(attributes, "index.ftl");
+            return new ModelAndView(attributes, "charts.ftl");
         }, freeMarkerEngine);
     }
 }
